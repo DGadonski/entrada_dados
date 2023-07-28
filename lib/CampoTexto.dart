dadd52643c6c2b73b5edda1f3621e9457255b975
DGadonski <derek.gadonski@gmail.com>
2023-07-28 17:01:13 -0300

    Implementados parâmetros da classe TextField.

--- lib/CampoTexto.dart
+++ lib/CampoTexto.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, prefer_const_constructors
+// ignore_for_file: file_names, prefer_const_constructors, avoid_print, sort_child_properties_last, prefer_interpolation_to_compose_strings, prefer_final_fields
 
 import 'package:flutter/material.dart';
 
@@ -10,6 +10,9 @@ class CampoTexto extends StatefulWidget {
 }
 
 class _CampoTextoState extends State<CampoTexto> {
+
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +22,42 @@ class _CampoTextoState extends State<CampoTexto> {
           color: Colors.white
         )),
       ),
-      body: Container(
-        padding: EdgeInsets.all(32),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Digite um valor:'
+              ),
+              //enabled: false,
+              //maxLength: 2,
+              //maxLengthEnforcement: MaxLengthEnforcement.none,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25
+              ),
+              //obscureText: true, utilizado em senhas.
+              /*onChanged: (String texto){
+                print('Texto digitado: $texto.');
+              },*/
+              onSubmitted:(String texto) {
+                print('Texto digitado: $texto.');
+              }, 
+              controller: _textEditingController,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: (){
+              print('O valor digitado é ' + _textEditingController.text);
+            }, 
+            child: Text('Salvar'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Colors.lightGreen)
+            ),
+            )
+        ],
       ),
     );
   }
